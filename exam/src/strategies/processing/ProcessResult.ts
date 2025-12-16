c1e331263cb209eb527c34bc94da68f6c0482b23
claudia.lajara <claudia.lajara@students.salle.url.edu>
2025-12-16 20:36:01 +0100

    Implemented processor strategies

--- exam/src/strategies/processing/ProcessResult.ts
+++ exam/src/strategies/processing/ProcessResult.ts
@@ -3,9 +3,6 @@ export class ProcessResult {
     private extractedData: Record<string, any>;
     private messages: string[];
 
-    private readonly SUCCESS_MESSAGE = "Document processed successfully!\n";
-    private readonly FAILURE_MESSAGE = "Document processing failed.\n";
-
     constructor(isSuccessful: boolean, extractedData: Record<string, any>, messages: string[]) {
         this.isSuccessful = isSuccessful;
         this.extractedData = extractedData;
@@ -15,39 +12,9 @@ export class ProcessResult {
     public getReport(): string {
         let report: string = "";
 
-        report.concat(this.reportSuccess());
-        report.concat(this.reportExtractedData());
-        report.concat(this.reportMessages());
-
-        return report;
-    }
-
-    private reportSuccess(): string {
-        if (this.isSuccessful) {
-            return this.SUCCESS_MESSAGE;
-        }
-
-        return this.FAILURE_MESSAGE;
-    }
-
-    private reportExtractedData(): string {
-        let report: string = "";
-
-        for (const data in this.extractedData) {
-            report.concat(data);
-            report.concat("\n");
-        }
-
-        return report;
-    }
-
-    private reportMessages(): string {
-        let report: string = "";
-
-        for (const message in this.messages) {
-            report.concat(message);
-            report.concat("\n");
-        }
+        report.concat(`Document type: ${this.extractedData["documentType"]}\n`);
+        report.concat(`Document name: ${this.extractedData["fileName"]}\n`);
+        report.concat(this.messages[0]);
 
         return report;
     }
--- exam/src/strategies/processing/ProposalProcessor.ts
+++ exam/src/strategies/processing/ProposalProcessor.ts
@@ -1,5 +1,16 @@
+import { Document } from "../../objects/Document";
 import { IDocumentProcessor } from "./IDocumentProcessor";
+import { ProcessResult } from "./ProcessResult";
 
 export class ProposalProcessor implements IDocumentProcessor {
-    //
+    process(document: Document): ProcessResult {
+        const messages: string[] = [];
+        const extractedData: Record<string, any> = {};
+
+        messages.push("Successfully processed document.");
+        extractedData["documentType"] = "Commercial Proposal";
+        extractedData["fileName"] = document.getFileName();
+
+        return new ProcessResult(true, extractedData, messages);
+    }
 }
