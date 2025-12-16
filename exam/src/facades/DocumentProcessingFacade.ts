160b0ae5daf252ef4cda93752a7ddf051b946b8b
Aldo Vignone <aldo.vignone@students.salle.url.edu>
2025-12-16 21:41:52 +0100

    add: document creation and validation

--- exam/src/facades/DocumentProcessingFacade.ts
+++ exam/src/facades/DocumentProcessingFacade.ts
@@ -1,8 +1,14 @@
+import { DocumentFactory } from "../factories/DocumentFactory";
+
+
 export class DocumentProcessingFacade {
   processDocument(fileName: string, documentType: string, metadata?: Record<string, string>) {
     // 1. Crear documento
+    const document = DocumentFactory.createDocument(documentType, fileName, metadata);
     // 2. Validar documento
+    document.validate();
     // 3. Procesar documento
+
     // 4. Retornar resultado del procesamiento
   }
 }
