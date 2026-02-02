cd1cd01adc48b143a9d1eed1a31abd1c434c3f0b
Jaume <jaume.vinyes@vistaprint.com>
2026-02-02 19:05:24 +0100

    Use the document factory

--- exam/src/facades/DocumentProcessingFacade.ts
+++ exam/src/facades/DocumentProcessingFacade.ts
@@ -6,16 +6,19 @@ import { Proposal } from "../models/Proposal";
 import { ProcessResult } from "../models/ProcessResult";
 import { DocumentProcessorFactory } from "../factories/DocumentProcessorFactory";
 import { ValidationException } from "../exceptions/ValidationException";
+import { DocumentFactory } from "../factories/DocumentFactory";
 
 export class DocumentProcessingFacade {
+  private documentFactory: DocumentFactory;
   private processorFactory: DocumentProcessorFactory;
 
   constructor() {
     this.processorFactory = new DocumentProcessorFactory();
+    this.documentFactory = new DocumentFactory();
   }
 
   processDocument(fileName: string, documentType: DocumentType, metadata?: Record<string, string>): ProcessResult {
-    const document = this.createDocument(fileName, documentType, metadata || {});
+    const document = this.documentFactory.createDocument(fileName, documentType, metadata || {});
 
     const isValid = document.isValid();
 
@@ -30,44 +33,5 @@ export class DocumentProcessingFacade {
     const allMessages = [...document.getValidationMessages(), ...result.getMessages()];
 
     return new ProcessResult(result.isSuccess(), result.getExtractedData(), allMessages);
-  }
-
-  private createDocument(fileName: string, documentType: DocumentType, metadata: Record<string, string>): Document {
-    const extension = this.extractExtension(fileName);
-    const sizeInKB = this.simulateFileSize(fileName);
-
-    switch (documentType) {
-      case "Contract":
-        return new Contract(fileName, extension, sizeInKB, {
-          author: metadata.author || "",
-          version: metadata.version || "",
-        });
-      case "FinancialReport":
-        return new FinancialReport(fileName, extension, sizeInKB, {
-          fiscalYear: metadata.fiscalYear || "",
-          department: metadata.department || "",
-        });
-      case "Proposal":
-        return new Proposal(fileName, extension, sizeInKB, {
-          proposalDate: metadata.proposalDate || "",
-          client: metadata.client || "",
-        });
-      default:
-        throw new Error(`Tipo de documento no soportado: ${documentType}`);
-    }
-  }
-
-  private extractExtension(fileName: string): string {
-    const lastDot = fileName.lastIndexOf(".");
-    if (lastDot === -1) {
-      return "";
-    }
-    return fileName.substring(lastDot);
-  }
-
-  private simulateFileSize(fileName: string): number {
-    const baseSize = 1024;
-    const variance = fileName.length * 10;
-    return baseSize + variance;
   }
 }
